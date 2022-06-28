ecec50a793ccc134159fcb41344522b5a5e7a2de
Evie Lau <evie@us.ibm.com>
2022-06-28 16:09:45 -0500

    Add comment check for completion to avoid breaking language server

--- liberty-ls/src/main/java/io/openliberty/tools/langserver/model/propertiesfile/PropertiesEntryInstance.java
+++ liberty-ls/src/main/java/io/openliberty/tools/langserver/model/propertiesfile/PropertiesEntryInstance.java
@@ -9,6 +9,7 @@
 *******************************************************************************/
 package io.openliberty.tools.langserver.model.propertiesfile;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -70,9 +71,12 @@ public class PropertiesEntryInstance {
     }
 
     public CompletableFuture<List<CompletionItem>> getCompletions(Position position) {
-        if (propertyValueInstance.toString() != null && !isOnEntryKey(position)) {
-            return propertyValueInstance.getCompletions(position);
-        } 
-        return propertyKeyInstance.getCompletions(position);
+        if (!isComment) {
+            if (propertyValueInstance.toString() != null && !isOnEntryKey(position)) {
+                return propertyValueInstance.getCompletions(position);
+            } 
+            return propertyKeyInstance.getCompletions(position);
+        }
+        return CompletableFuture.completedFuture(Collections.emptyList());
     }
 }
