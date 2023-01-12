cda437198f2cc4dd61135affe6d3d558c05e668c
dshimo <dshi@utexas.edu>
2023-01-12 11:25:32 -0600

    copyright headers

--- lemminx-liberty/src/main/java/io/openliberty/tools/langserver/lemminx/util/LibertyConstants.java
+++ lemminx-liberty/src/main/java/io/openliberty/tools/langserver/lemminx/util/LibertyConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2020, 2022 IBM Corporation and others.
+* Copyright (c) 2020, 2022, 2023 IBM Corporation and others.
 *
 * This program and the accompanying materials are made available under the
 * terms of the Eclipse Public License v. 2.0 which is available at
@@ -11,8 +11,6 @@
 *     IBM Corporation - initial API and implementation
 *******************************************************************************/
 package io.openliberty.tools.langserver.lemminx.util;
-
-import java.io.File;
 
 public final class LibertyConstants {
     private LibertyConstants() {
--- lemminx-liberty/src/main/java/io/openliberty/tools/langserver/lemminx/util/LibertyUtils.java
+++ lemminx-liberty/src/main/java/io/openliberty/tools/langserver/lemminx/util/LibertyUtils.java
@@ -1,5 +1,5 @@
- /*******************************************************************************
-* Copyright (c) 2020, 2022 IBM Corporation and others.
+/*******************************************************************************
+* Copyright (c) 2020, 2022, 2023 IBM Corporation and others.
 *
 * This program and the accompanying materials are made available under the
 * terms of the Eclipse Public License v. 2.0 which is available at
@@ -58,7 +58,7 @@ public class LibertyUtils {
 
     public static boolean isServerXMLFile(DOMDocument file) {
         return file.getDocumentURI().endsWith("/" + LibertyConstants.SERVER_XML);
-    } 
+    }
 
     public static boolean isConfigDirFile(String filePath) {
         return filePath.contains(LibertyConstants.WLP_USER_CONFIG_DIR) ||
