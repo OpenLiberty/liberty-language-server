531dd11aa81775915e99704ff3b57a22d41d4707
dshimo <dshi@utexas.edu>
2023-08-14 11:56:15 -0500

    make indentation uniform; 4 spaces

--- lemminx-liberty/src/main/java/io/openliberty/tools/langserver/lemminx/models/settings/AllSettings.java
+++ lemminx-liberty/src/main/java/io/openliberty/tools/langserver/lemminx/models/settings/AllSettings.java
@@ -19,15 +19,15 @@ import org.eclipse.lsp4j.jsonrpc.json.adapters.JsonElementTypeAdapter;
  * Model for top level xml settings JSON object.
  */
 public class AllSettings {
-  @JsonAdapter(JsonElementTypeAdapter.Factory.class)
-  private Object liberty;
+    @JsonAdapter(JsonElementTypeAdapter.Factory.class)
+    private Object liberty;
 
-  public Object getLiberty() {
-    return liberty;
-  }
+    public Object getLiberty() {
+        return liberty;
+    }
 
-  public void setLiberty(Object liberty) {
-    this.liberty = liberty;
-  }
+    public void setLiberty(Object liberty) {
+        this.liberty = liberty;
+    }
 
 }
