00e4bfa44154b814442e8c5f098471e7daa29546
dshimo <dshi@utexas.edu>
2023-08-31 10:38:17 -0500

    change config file logic (#194)
    * config logic swap

--- lemminx-liberty/src/test/java/io/openliberty/XmlReaderTest.java
+++ lemminx-liberty/src/test/java/io/openliberty/XmlReaderTest.java
@@ -5,34 +5,42 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
+import io.openliberty.tools.langserver.lemminx.util.LibertyUtils;
 import io.openliberty.tools.langserver.lemminx.util.XmlReader;
 
 public class XmlReaderTest {
     File resourcesDir = new File("src/test/resources");
 
     @Test
-    public void readEmptyXml() {
+    public void readEmptyXml() throws IOException {
         File emptyXml = new File(resourcesDir, "empty_server.xml");
-        assertFalse(XmlReader.hasServerRoot(emptyXml));
+        assertFalse(XmlReader.hasServerRoot(emptyXml.getCanonicalPath()));
+        assertFalse(LibertyUtils.isServerXMLFile(emptyXml.getCanonicalPath()));
+        assertFalse(LibertyUtils.isConfigXMLFile(emptyXml.getCanonicalPath()));
     }
 
     @Test
-    public void readServerXml() {
-        File sampleServerXml = new File(resourcesDir, "sample/server.xml");
-        assertTrue(XmlReader.hasServerRoot(sampleServerXml));
+    public void readServerXml() throws IOException {
+        File sampleServerXml = new File(resourcesDir, "sample/custom_server.xml");
+        assertTrue(XmlReader.hasServerRoot(sampleServerXml.getCanonicalPath()));
+        assertFalse(LibertyUtils.isServerXMLFile(sampleServerXml.getCanonicalPath()));
+        assertFalse(LibertyUtils.isConfigDirFile(sampleServerXml.getCanonicalPath()));
+        assertTrue(LibertyUtils.isConfigXMLFile(sampleServerXml.getCanonicalPath()));
     }
 
     @Test
-    public void readLibertyPluginConfigXml() {
+    public void readLibertyPluginConfigXml() throws IOException {
         File lpcXml = new File(resourcesDir, "sample/liberty-plugin-config.xml");
         Path lpcXmlPath = lpcXml.toPath();
-        assertFalse(XmlReader.hasServerRoot(lpcXml));
+        assertFalse(XmlReader.hasServerRoot(lpcXml.getCanonicalPath()));
+        assertFalse(LibertyUtils.isConfigXMLFile(lpcXml.getCanonicalPath()));
 
         Set<String> elementNames = new HashSet<String> ();
         elementNames.add("configFile");
