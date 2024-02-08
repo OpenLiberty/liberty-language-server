52da2beecb82a2d905688e6600706bc8b55bbcd6
dshimo <dshi@utexas.edu>
2024-02-08 13:19:41 -0600

    account for ci.common changes

--- liberty-ls/src/main/java/io/openliberty/tools/langserver/utils/ServerConfigUtil.java
+++ liberty-ls/src/main/java/io/openliberty/tools/langserver/utils/ServerConfigUtil.java
@@ -9,24 +9,22 @@ import java.util.logging.Logger;
 import javax.xml.xpath.XPathExpressionException;
 
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import io.openliberty.tools.common.plugins.config.ServerConfigDocument;
 import io.openliberty.tools.langserver.CommonLogger;
 
 //TODO: rename class to relate to ci.common
 public class ServerConfigUtil {
-    
-    private static final Logger LOGGER = Logger.getLogger(ServerConfigUtil.class.getName());
-    static CommonLogger log = CommonLogger.getInstance(LOGGER);
+    private final static Logger LOGGER = Logger.getLogger(ServerConfigUtil.class.getName());
+    private static CommonLogger log = CommonLogger.getInstance(LOGGER);
+    private static ServerConfigDocument configDocument = new ServerConfigDocument(log);
 
     // need method to request ci.common to parse vars from an xml file
     public static void requestParseXml(String uri) {
-        ServerConfigDocument.setLogger(log);
         Document doc;
         try {
-            doc = ServerConfigDocument.parseDocument(new File(URI.create(uri)));
-            ServerConfigDocument.parseVariablesForBothValues(doc);
+            doc = configDocument.parseDocument(new File(URI.create(uri)));
+            configDocument.parseVariablesForBothValues(doc);
         } catch (XPathExpressionException | IOException e) {
             // TODO Auto-generated catch block
             LOGGER.warning("Error trying to parse document: " + uri);
@@ -36,6 +34,6 @@ public class ServerConfigUtil {
 
     // need method to request Properties from ci.common
     public static Properties getProperties() {
-        return ServerConfigDocument.getProperties();
+        return configDocument.getProperties();
     }
 }
