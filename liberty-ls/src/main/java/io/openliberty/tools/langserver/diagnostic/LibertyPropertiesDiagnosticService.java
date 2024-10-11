08ee52a395c179fd8358409e6bf269b310ad05d0
arunvenmany-ibm <arun.kumar.v.n@ibm.com>
2024-10-11 20:20:47 +0530

    Server env blank line issue fix (#310)
    * Fixing blank line issue
    
    * Fixing blank line issue
    
    Signed-off-by: Arun Venmany <Arun.Kumar.V.N@ibm.com>
    
    ---------
    
    Signed-off-by: Arun Venmany <Arun.Kumar.V.N@ibm.com>

--- liberty-ls/src/main/java/io/openliberty/tools/langserver/diagnostic/LibertyPropertiesDiagnosticService.java
+++ liberty-ls/src/main/java/io/openliberty/tools/langserver/diagnostic/LibertyPropertiesDiagnosticService.java
@@ -49,9 +49,12 @@ public class LibertyPropertiesDiagnosticService  {
             int lineNumber = 0;
             try {
                 while ((line=br.readLine()) != null) {
-                    PropertiesValidationResult validationResult = PropertiesValidationResult.validateServerProperty(line, openedDocument, lineNumber);
-                    if (validationResult.hasErrors()) {
-                        errors.put(line, validationResult);
+                    //blank line is being ignored
+                    if(!line.isBlank()) {
+                        PropertiesValidationResult validationResult = PropertiesValidationResult.validateServerProperty(line, openedDocument, lineNumber);
+                        if (validationResult.hasErrors()) {
+                            errors.put(line, validationResult);
+                        }
                     }
                     lineNumber++;
                 }
