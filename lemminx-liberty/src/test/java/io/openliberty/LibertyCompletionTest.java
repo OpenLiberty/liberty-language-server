1437db5a81392034d055c8e554c901e6e5f01890
Arun Venmany <Arun.Kumar.V.N@ibm.com>
2024-10-22 22:14:25 +0530

    fixing feature completion issue for repeating same feature
    Signed-off-by: Arun Venmany <Arun.Kumar.V.N@ibm.com>

--- lemminx-liberty/src/test/java/io/openliberty/LibertyCompletionTest.java
+++ lemminx-liberty/src/test/java/io/openliberty/LibertyCompletionTest.java
@@ -206,6 +206,22 @@ public class LibertyCompletionTest {
                 // one for CDATA and one for <-
                 sipServletCompletionItem = c("sipServlet-1.1", "sipServlet-1.1");
                 XMLAssert.testCompletionFor(serverXML, null, serverXMLURI, 3, sipServletCompletionItem);
+
+                serverXML = String.join(newLine, //
+                        "<server description=\"Sample Liberty server\">", //
+                        "       <featureManager>", //
+                        "               <feature>servlet-</feature>", //
+                        "               <feature>servlet</feature>", //
+                        "               <feature>servlet|</feature>", //
+                        "       </featureManager>", //
+                        "</server>" //
+                );
+
+                // total number of available completion items
+                // 1 for sipServlet-1.1
+                // one for CDATA and one for <-
+                sipServletCompletionItem = c("sipServlet-1.1", "sipServlet-1.1");
+                XMLAssert.testCompletionFor(serverXML, null, serverXMLURI, 3, sipServletCompletionItem);
         }
 
 }
