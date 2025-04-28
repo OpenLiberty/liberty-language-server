8a8011b759fdf13db7ad7c15c96e20b2ee394026
Cheryl King <cheryllk@us.ibm.com>
2025-04-28 16:34:01 -0500

    fix code

--- lemminx-liberty/src/main/java/io/openliberty/tools/langserver/lemminx/services/FeatureService.java
+++ lemminx-liberty/src/main/java/io/openliberty/tools/langserver/lemminx/services/FeatureService.java
@@ -147,7 +147,7 @@ public class FeatureService {
             if (defaultFeaturesAndPlatforms == null) {
                 // Changing this to contain the version in the file name since the file is copied to the local .lemminx cache. 
                 // This is how we ensure the latest default features json gets used in each developer environment. 
-                InputStream is = getClass().getClassLoader().getResourceAsStream("features-cached-25.0.0.3.json");
+                InputStream is = getClass().getClassLoader().getResourceAsStream("features-cached-25.0.0.4.json");
                 InputStreamReader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
 
                 // Only need the public features
